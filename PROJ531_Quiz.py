deeb0dd3f7cfb49836c99b35c503773f63b7b5b3
ValentinPB <122006974+ValentinPB@users.noreply.github.com>
2023-01-05 09:51:56 +0100

    Quizs Tests
    Ajout du dossier Quizs avec les .txt de test, et ajout des exemples de représentation de quiz sur le code.

--- PROJ531_Quiz.py
+++ PROJ531_Quiz.py
@@ -1,3 +1,15 @@
 ##PROJ531 Quiz
 
-#ceci est un test
+with open('C:\Users\Valentin\Documents\GitHub\PROJ531\Quizs\QuizTest.txt') as q :
+    content = q.readlines()
+close(q)
+
+print(content)
+
+
+questions = [q1, Q2]
+rep = [[r11, r21], [r21, r22, r23, r24]]
+repcorr = [[O,1], [0,0,1,0]]
+
+Q = [[q1, [[r11, 0], [r12, 1]]], [q2, [[r21, 0], [r22, 0], [r23, 1], [r24, 0]]]]
+
