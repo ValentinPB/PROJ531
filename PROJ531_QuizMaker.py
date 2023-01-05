f5534529b4922e0427e2e3b7caff9bc7a10453cf
ValentinPB <122006974+ValentinPB@users.noreply.github.com>
2023-01-05 17:06:33 +0100

    Cleanup et init QuizMaker
    Nettoyage du fichier de code où moi et Loïs avions push ensemble, maintenant qu'il code sur son propre fichier.
    Ajout du squelette du fichier QuizMaker qui servira à créer des fichiers txt de manière simple pour les administrateurs.

---
+++ PROJ531_QuizMaker.py
@@ -0,0 +1,16 @@
+##PROJ531 Quiz Création fichier
+
+def CreateFile(titre) :
+    pass
+
+def IsFileEmpty() :
+    pass
+
+def FillFile(titre, questions, reponses, corrections, scores, timers) :
+    pass
+
+def IsAdmin() :
+    pass
+
+def MakeQuiz() :
+    pass
