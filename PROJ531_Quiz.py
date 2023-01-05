20e198bf807387aae0a2ff8e900a09202c074531
ValentinPB <122006974+ValentinPB@users.noreply.github.com>
2023-01-05 11:07:15 +0100

    Read quiz txt
    Le code sait maintenant lire un .txt formatté selon nos conventions et le transformer en listes explicites et exploitables.

--- PROJ531_Quiz.py
+++ PROJ531_Quiz.py
@@ -1,15 +1,23 @@
 ##PROJ531 Quiz
 
-with open('C:\Users\Valentin\Documents\GitHub\PROJ531\Quizs\QuizTest.txt') as q :
+with open(r'C:\Users\Valentin\Documents\GitHub\PROJ531\Quizs\QuizTest.txt') as q :
     content = q.readlines()
-close(q)
+q.close
 
-print(content)
+print(content[0])
 
+titre, questions, rep, repcorr = content[0].split(';')
 
-questions = [q1, Q2]
-rep = [[r11, r21], [r21, r22, r23, r24]]
-repcorr = [[O,1], [0,0,1,0]]
+questions = questions.split(',')
 
-Q = [[q1, [[r11, 0], [r12, 1]]], [q2, [[r21, 0], [r22, 0], [r23, 1], [r24, 0]]]]
+reponses = []
+rep = rep.split(',')
+for i in rep :
+    #print(reponses)
+    #print(i.split('.'))
+    reponses.append(i.split('.'))
 
+reponsescorrectes = []
+repcorr = repcorr.split(',')
+for i in repcorr :
+    reponsescorrectes.append(i.split('.'))
