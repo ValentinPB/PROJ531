03ece9762fc041906bc67a899439099943e6c7e4
loisblin <113303000+loisblin@users.noreply.github.com>
2023-01-10 08:45:04 +0100

    Update Quizs play basique programme.py

--- Quizs play basique programme.py
+++ Quizs play basique programme.py
@@ -1,10 +1,10 @@
 class quiz:
-    def __init__(self,questions,rep,repcorr,point):
+    def __init__(self,questions,rep,repcorr,point,temps):
         self.questions=questions                        # liste questions 
         self.rep=rep                                    #liste de liste de réponses pour chaque questions 
         self.repcorr=repcorr                            # liste avec binaire qui indique la position des bonne réponse 
         self.point=point                                #nombre de point de la question 
-        
+        self.temps=temps
     def affichequestion(self,numeros):
         print (self.questions[numeros] )
         return  (self.questions[numeros] )
@@ -14,14 +14,17 @@ class quiz:
         return (self.rep[numeros])
     
     def afficherepcorrect(self,numeros):
+        Listebonnerep=[]
         for k in range(0,len(self.repcorr[numeros])):
             if self.repcorr[numeros][k]==1:
-                print(k+1)
-                return k+1
+                Listebonnerep.append(k+1)
+                
+        print(Listebonnerep)
+        return Listebonnerep
     
-    def donner_une_rep(self,numeros):
-        reponse_user=input('reponse')
-        return int(reponse_user)
+    def donner_une_rep(self):
+        
+        return input('liste réponse correcte')
 
 
     def comparaison_rep(self,rep_user,numeros):
@@ -58,10 +61,13 @@ class quiz:
 
 testquestions = ['q1', 'Q2']
 testrep = [['r11', 'r21'], ['r21', 'r22', 'r23', 'r24']]
-testrepcorr = [[0,1], [0,0,1,0]]
+testrepcorr = [[0,1], [0,0,1,1]]
 testpoint=[1, 2]
+testtemps=[30,45]
 
-testquiz=quiz(testquestions,testrep,testrepcorr,testpoint)
+testquiz=quiz(testquestions,testrep,testrepcorr,testpoint,testtemps)
+testquiz.afficherepcorrect(1)
+r=testquiz.donner_une_rep()
+print(r)
+#testquiz.partie()
 
-testquiz.partie()
-h=2
