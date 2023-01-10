cc74cd60c41e6f73f5e57ab7a666c83b0a422f82
Bob <yassir.chafik2000@gmail.com>
2023-01-10 10:02:29 +0100

    interface graphique  on a les deux fenetres

---
+++ interface_graphique/interface de connexion + choix quiz.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 08:37:49 2023
+
+@author: chafi
+"""
+
+from tkinter import *
+global fenetre1, fenetre2
+
+def test():
+    global fenetre1, fenetre2
+    fenetre1.destroy()
+    choix_quiz()
+
+def fenetre():
+    global fenetre1
+    
+    
+
+
+
+
+    def getEntry():
+        Id = entree_name.get()
+        mdp = password_entry.get()
+        return (Id)
+    
+    
+    fenetre1= Tk ()
+    obj = Label(fenetre1,text = "bonjour")
+    
+    name_user =Label( fenetre1,text ="nom d'utilisateur")
+    name_user.pack()
+    entree_name = Entry()
+    entree_name.pack()
+    psw= Label(fenetre1,text = "mot de passe")
+    password_entry = Entry()
+    psw.pack()
+    password_entry.pack()
+    
+    bouton_connexion = Button(fenetre1,text= "connexion", command=test)  #ici on doit mettre une commande qui verifie l'utilisateur avant de destroy()  
+    bouton_connexion.pack()
+    
+    obj.pack()
+    obj.place(x=10, y=50)
+    fenetre1.geometry("800x900")
+    
+    fenetre1.mainloop()
+
+
+    """fenetre1 = Tk()
+    champ_label = Label(fenetre1, text="Salut !")
+    champ_label.pack()
+    bouton = Button(fenetre1,text="Acceuil", command=test)
+    bouton.pack()
+    fenetre1.mainloop()"""
+
+def choix_quiz():
+    global fenetre2
+    fenetre2 = Tk()
+    champ_label = Label(fenetre2, text="choisir un quiz")
+    champ_label.place(x=10, y=10 , anchor='ne')
+    champ_label.pack()
+    bouton = Button(fenetre2,text="quiz1", command=test)
+    bouton.pack()
+    bouton2 = Button(fenetre2,text="quiz2", command=test)
+    bouton2.pack()
+    bouton3 = Button(fenetre2,text="quiz3", command=test)
+    bouton3.pack()
+    fenetre2.geometry("800x900")
+    fenetre2.mainloop()
+    
+    
+if __name__ == '__main__':
+    fenetre()
