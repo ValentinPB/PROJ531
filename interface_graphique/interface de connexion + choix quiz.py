0a8de1835c6aebcf66fdf64438fe3e4ce06d2849
Bob <yassir.chafik2000@gmail.com>
2023-01-12 09:29:34 +0100

    ajout des fenetres necessaires pour l'admin

--- interface_graphique/interface de connexion + choix quiz.py
+++ interface_graphique/interface de connexion + choix quiz.py
@@ -6,7 +6,8 @@ Created on Tue Jan 10 08:37:49 2023
 """
 import authentification as aut
 from tkinter import *
-global fenetre1, fenetre2
+global fenetre1, fenetre2,administrateur
+
 
 
     
@@ -34,18 +35,31 @@ def fenetre():
         Id = entree_name.get()
         mdp = password_entry.get()
         connect = aut.authentification(Id, mdp)
+        
         if connect:
             fenetre1.destroy()
+            choix_quiz()
         else:
-            bjr = Label(app, text="pppppppppppppppppppppppppppp")
-            bjr.pack()
-        choix_quiz()
+            erreur = Label(fenetre1, text="nom utilisateur ou mdp erronés")
+            erreur.pack()
+    def test1():
+        global fenetre1, auth_administrateur
+        fenetre1.destroy()
+        page_auth_administrateur()
+        
     
 
-
-
-    
     fenetre1= Tk ()
+    menu= Menu(fenetre1)
+    new_item = Menu(menu)
+    new_item.add_command(label='admin', command=test1)
+    new_item.add_separator()
+    menu.add_cascade(label='Fichier' , menu=new_item)
+    fenetre1.config(menu=menu)
+    
+    
+    
+    
     obj = Label(fenetre1,text = "Bonjour")
     
     name_user =Label( fenetre1,text ="nom d'utilisateur")
@@ -57,11 +71,12 @@ def fenetre():
     psw.pack()
     password_entry.pack()
     
-    bouton_connexion = Button(fenetre1,text= "connexion", command=test)  #ici on doit mettre une commande qui verifie l'utilisateur avant de destroy()  
+    bouton_connexion = Button(fenetre1,text= "connexion", command=test)    
     bouton_connexion.pack()
     
+    
     obj.pack()
-    obj.place(x=10, y=10 , anchor='ne')
+    obj.place(x=300, y=10 , anchor='ne')
     fenetre1.geometry("800x900")
     
     fenetre1.mainloop()
@@ -76,7 +91,7 @@ def fenetre():
 
 def choix_quiz():
     global fenetre2
-    def test():
+    def test(): # a modifier selon quel fenetre on veut ouvrir
         choix_quiz()
     fenetre2 = Tk()
     champ_label = Label(fenetre2, text="choisir un quiz")
@@ -91,6 +106,47 @@ def choix_quiz():
     fenetre2.geometry("800x900")
     fenetre2.mainloop()
     
+def page_auth_administrateur():
+    global auth_administrateur
+    def test3():
+        global auth_administrateur , admin
+        Id = entree_name.get()
+        mdp = password_entry.get()
+        connect = aut.est_admin(Id, mdp)
+        
+        if connect:
+            auth_administrateur.destroy()
+            page_admin()
+        else:
+            erreur = Label(fenetre1, text="nom utilisateur ou mdp erronés")
+            erreur.pack()
+    auth_administrateur = Tk()
     
+    
+    obj = Label(auth_administrateur,text = "Bonjour admin")
+    obj.pack()
+    obj.place(x=300, y=10 , anchor='ne')
+    name_user =Label( auth_administrateur,text ="nom d'utilisateur")
+    name_user.pack()
+    entree_name = Entry(auth_administrateur)
+    entree_name.pack()
+    psw= Label(auth_administrateur,text = "mot de passe")
+    password_entry = Entry(auth_administrateur)
+    psw.pack()
+    password_entry.pack()
+    
+    bouton_connexion = Button(auth_administrateur,text= "connexion", command=test3)    
+    bouton_connexion.pack()
+    
+
+    
+    auth_administrateur.geometry("800x900")
+    auth_administrateur.mainloop()
+    
+def page_admin():
+    global admin
+    admin=Tk()
+    admin.geometry("800x900")
+    admin.mainloop()
 if __name__ == '__main__':
     fenetre()
