aef872e3f34fbcfabf0199e95c48dfb131e29667
coltarm <113551889+coltarm@users.noreply.github.com>
2023-01-12 08:46:30 +0100

    ajout inter.py

--- interface_graphique/interf.py
+++ interface_graphique/interf.py
@@ -1,10 +1,22 @@
-import tkinter as tk
-app= tk.Tk ()
-obj = tk.Label(text = "bonjour")
+from tkinter import *
 
-obj.pack()
-obj.place(x=10, y=50)
-app.geometry("800x900")
+class Application(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.creer_widgets()
 
-app.mainloop()
+    def creer_widgets(self):
+        question_suiv = "question"
+        self.label = Label(self, text="Quizz")
+        self.bouton = Button(self, text= question_suiv, command=self.question_suivante)
+        self.label.pack()
+        self.bouton.pack()
 
+    def question_suivante(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.geometry("800x600")
+    app.mainloop()
